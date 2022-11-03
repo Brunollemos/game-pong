d0c6d333c22b516dd25a1eb362c27c4a374d586d
Bruno Lemos <brunollemoscontato@gmail.com>
2022-11-03 04:32:20 -0300

    Criando e chamando função mostraBolinha

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ function setup() {
 
 function draw() {
   background(0);
+  mostraBolinha();
 }
 
 //varáveis da bolinha
@@ -11,3 +12,7 @@ let xBolinha = 300;
 let yBolinha = 200;
 let diametro = 13;
 let raio = diametro / 2;
+
+function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametro);
+}
